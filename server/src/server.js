7415be826c00459fce8a220e85cd2390edc57cbe
Nikita Budiakov <budyakov91@gmail.com>
2025-06-16 20:24:38 -0400

    add base code for running server and client

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,29 @@
-require('dotenv').config();
-const express = require('express');
+import express from "express";
+import cors from "cors";
+import xss from 'xss-clean'
+import helmet from 'helmet';
+import rateLimiter from 'express-rate-limit';
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+
+dotenv.config();
 const app = express();
 
-// extra security packages
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
-
 // connect DB
-const connectDB = require('./config/connect');
-const authenticateUser = require('./middleware/authentication');
+// const authenticateUser = require('./middleware/authentication');
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+// app.use(xss());
+
+app.route('/').get((req, res) => res.send('<h1>Welcome</h1>'));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+      app.listen(PORT, () => console.log(`Listening on port: ${ PORT }`))
+  })
+  .catch((err) => console.log(err));
+
