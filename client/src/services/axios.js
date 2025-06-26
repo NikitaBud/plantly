5b2e3cbe76d305fc03698eabf46473d26fa5d3b7
Nikita Budiakov <budyakov91@gmail.com>
2025-06-26 11:57:55 -0400

    fix: edited base path

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ?
-    process.env.REACT_APP_API_URL + '/api/v1' : '/api/v1',
+    process.env.REACT_APP_API_URL : '/api/v1',
   withCredentials: true,
 });
 
